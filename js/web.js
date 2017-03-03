d3b5ea1d43f044446ee1bd4cfb53c316009f9b71
karthik5198 <jakkampudi.karthik19@gmail.com>
2017-03-03 09:26:19 -0600

    fif

--- js/web.js
+++ js/web.js
@@ -1,164 +1,148 @@
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+window.addEventListener("load", windowLoadHandler, false);
 
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+function windowLoadHandler() {
+  canvasApp();
+}
 
-// MIT license
-
-(function() {
-  var lastTime = 0;
-  var vendors = ['ms', 'moz', 'webkit', 'o'];
-  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+function canvasApp() {
+  
+  var displayCanvas = document.getElementById("displayCanvas");
+  var context = displayCanvas.getContext("2d");
+  
+  var particleList;
+  var numParticles;
+  var displayWidth;
+  var displayHeight;
+  var timer;
+  var p;
+  var particleColor;
+  var maxParticleRad;
+  var minParticleRad;
+  var initVelMax;
+  var maxVelComp;
+  var randAccel;
+  var maxParticleRad;
+  var minParticleRad;
+  var fadeColor;
+  
+  init();
+  
+  function init() {
+    
+    numParticles = 50;
+    
+    maxParticleRad = 6;
+    minParticleRad = 3;
+    
+    displayWidth = displayCanvas.width;
+    displayHeight = displayCanvas.height;
+    
+    initVelMax = 1.5;
+    maxVelComp = 2.5;
+    randAccel = 0.2;
+        
+    particleList = {};
+    createParticles();
+    
+    fadeColor = "rgba(0,0,0,0.1)";
+    
+    context.fillStyle = "#050505";
+    context.fillRect(0,0,displayWidth,displayHeight);
+    
+    timer = setInterval(onTimer, 1000/40);
   }
-
-  if (!window.requestAnimationFrame)
-    window.requestAnimationFrame = function(callback, element) {
-      var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      var id = window.setTimeout(function() {
-          callback(currTime + timeToCall);
-        },
-        timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-
-  if (!window.cancelAnimationFrame)
-    window.cancelAnimationFrame = function(id) {
-      clearTimeout(id);
-    };
-}());
-
-// ends requestAnimationFrame polyfill
-
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
-
-var cw = c.width = window.innerWidth;
-var ch = c.height = window.innerHeight;
-// para calcular el radio
-var a = 10;
-var b = 30;
-// dot radius
-var r = 3;
-// how many dots
-var howMany = 40;
-var particlesRy = [];
-//ctx.globalAlpha = .5;
-ctx.lineWidth = 2;
-
-function Particles(a, b) {
-
-  this.pm = Math.random() < 0.5 ? -1 : 1; //plus or minus
-
-  this.drift = this.pm * Math.round(Math.random() * 8) + 1;
-  this.rise = -1 * Math.round(Math.random() * 8) + 1;
-
-  this.hue = Math.round(Math.random() * 360) + 1;
-  this.color = "hsl(" + this.hue + ",100%, 60%)";
-
-  this.x = Math.round(Math.random() * cw) + 1;
-  this.y = Math.round(Math.random() * ch) + 1;
-}
-
-for (var i = 0; i < howMany; i++) {
-  createParticles(i);
-}
-
-function createParticles(i) {
-  particlesRy[i] = new Particles(a, b);
-  compare(i);
-}
-
-function buildDot(j) {
-  ctx.beginPath();
-  ctx.fillStyle = particlesRy[j].color;
-  ctx.strokeStyle = particlesRy[j].color;
-  ctx.moveTo(particlesRy[j].x + r, particlesRy[j].y);
-  ctx.arc(particlesRy[j].x, particlesRy[j].y, r, 0, 2 * Math.PI);
-  ctx.shadowBlur = 0;
-  ctx.shadowColor = particlesRy[j].color;
-  ctx.fill();
-}
-
-function Update() {
-
-  for (var j = 0; j < particlesRy.length; j++) {
-    particlesRy[j].y += particlesRy[j].rise;
-    particlesRy[j].x += particlesRy[j].drift;
-
-    if (particlesRy[j].drift == 0) {
-      particlesRy[j].drift += 2 * particlesRy[j].pm;
-    }
-    if (particlesRy[j].rise == 0) {
-      particlesRy[j].rise += 2 * particlesRy[j].pm;
-    }
-
-    if (particlesRy[j].y <= 0 ||  particlesRy[j].y >= ch) {
-      particlesRy[j].rise = -1 * particlesRy[j].rise;
-      /*particlesRy[j].rise+=.5;*/
-    }
-
-    if (particlesRy[j].x <= 0 || particlesRy[j].x >= cw) {
-      particlesRy[j].drift = -1 * particlesRy[j].drift;
-    }
-
-    compare(j);
-
-    ctx.shadowBlur = 0;
-
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.04)';
-    ctx.fillRect(0, 0, cw, ch);
-  }
-
-  requestId = window.requestAnimationFrame(Update);
-}
-
-function compare(n) {
-
-  for (var c = 0; c < particlesRy.length; c++) {
-    if (n !== c) {
-
-      var cx = Math.abs(particlesRy[c].x - particlesRy[n].x);
-      var cy = Math.abs(particlesRy[c].y - particlesRy[n].y);
-      var hyp = Math.sqrt(cx * cx + cy * cy);
-
-      if (hyp < 200) {
-        var alpha = (100 / hyp <= 1) ? 100 / hyp : 1;
-        var grd = ctx.createLinearGradient(particlesRy[n].x, particlesRy[n].y, particlesRy[c].x, particlesRy[c].y);
-        grd.addColorStop(0, "hsla(" + particlesRy[n].hue + ",100%, 60%," + alpha + ")");
-        grd.addColorStop(1, "hsla(" + particlesRy[c].hue + ",100%, 60%," + alpha + ")");
-        ctx.strokeStyle = grd;
-        ctx.shadowBlur = 20;
-        ctx.shadowColor = grd;
-        ctx.beginPath();
-        ctx.moveTo(particlesRy[n].x, particlesRy[n].y);
-        ctx.lineTo(particlesRy[c].x, particlesRy[c].y);
-        ctx.stroke();
+  
+  function createParticles() {
+    var angle;
+    var dist;
+    var vAngle;
+    var vMag;
+    var r,g,b;
+    var minRGB = 16;
+    var maxRGB = 255;
+    var alpha = 1;
+    var color;
+    for (var i = 0; i < numParticles; i++) {
+      angle = Math.random()*2*Math.PI;
+      vAngle = Math.random()*2*Math.PI;
+      vMag = initVelMax*(0.6 + 0.4*Math.random());
+      r = Math.floor(minRGB + Math.random()*(maxRGB-minRGB));
+      g = Math.floor(minRGB + Math.random()*(maxRGB-minRGB));
+      b = Math.floor(minRGB + Math.random()*(maxRGB-minRGB));
+      color = "rgba(" + r + "," + g + "," + b + ","+ alpha + ")";
+      var newParticle = { x: Math.random()*displayWidth,
+                y: Math.random()*displayHeight,
+                velX: vMag*Math.cos(vAngle),
+                velY: vMag*Math.sin(vAngle),
+                rad: minParticleRad+Math.random()*(maxParticleRad-minParticleRad),
+                color:color}
+      if (i > 0) {
+        newParticle.next = particleList.first;
       }
-
+      particleList.first = newParticle;       
     }
-
-    buildDot(n);
   }
-
-}
-
-function start() {
-  requestId = window.requestAnimationFrame(Update);
-  stopped = false;
-}
-
-function stopAnim() {
-  if (requestId) {
-    window.cancelAnimationFrame(requestId);
+    
+  function onTimer() {
+    
+    //fading. This won't work very well in Chrome, IE, and Firefox - gray trails will be left behind.
+    context.fillStyle = fadeColor;
+    context.fillRect(0,0,displayWidth,displayHeight);
+    
+    //update and draw particles
+    p = particleList.first;
+    while (p != null) {
+      //update
+      lastX = p.x;
+      lastY = p.y;
+      
+      //random accleration
+      p.velX += (1-2*Math.random())*randAccel;
+      p.velY += (1-2*Math.random())*randAccel;
+      
+      //don't let velocity get too large
+      if (p.velX > maxVelComp) {
+        p.velX = maxVelComp;
+      }
+      else if (p.velX < -maxVelComp) {
+        p.velX = -maxVelComp;
+      }
+      if (p.velY > maxVelComp) {
+        p.velY = maxVelComp;
+      }
+      else if (p.velY < -maxVelComp) {
+        p.velY = -maxVelComp;
+      }
+      
+      p.x += p.velX;
+      p.y += p.velY;
+      
+      //boundary
+      if (p.x > displayWidth-p.rad) {
+        p.x = displayWidth-p.rad;
+        p.velX *= -1;
+      }
+      if (p.x < p.rad) {
+        p.x = p.rad;
+        p.velX *= -1;
+      }
+      if (p.y > displayHeight-p.rad) {
+        p.y = displayHeight-p.rad;
+        p.velY *= -1;
+      }
+      if (p.y < p.rad) {
+        p.y = p.rad;
+        p.velY *= -1;
+      }
+            
+      context.fillStyle = p.color;
+      context.beginPath();
+      context.arc(p.x, p.y, p.rad, 0, 2*Math.PI, false);
+      context.closePath();
+      context.fill();
+      
+      //advance
+      p = p.next;
+    }   
   }
-  stopped = true;
 }
-
-window.addEventListener("load", start(), false);
-c.addEventListener("click", function() {
-  (stopped == true) ? start(): stopAnim();
-}, false);
